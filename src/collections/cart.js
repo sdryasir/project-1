3047dea402c9e34853266098dca4b0f842159224
Yasir Ali <sdr.yasir@gmail.com>
2021-01-31 01:37:16 +0500

    remove item from the cart functionality achieved

--- src/collections/cart.js
+++ src/collections/cart.js
@@ -1,30 +1,63 @@
-import { cartdata } from "../data/cartdata";
+//import { cartdata } from "../data/cartdata";
 //import CartItem from "../components/cartItem";
 import { useState } from 'react'
+import cartImg from '../empty-cart.png'
 
 function Cart() {
-    const [qty, setQty] = useState(cartdata)
+    const [qty, setQty] = useState([
+        {
+            id: 1,
+            title: "Krunch Burger ",
+            image: "/images/products/Bone-kfc.png",
+            description: "Enjoy the crispy chicken fillet in a soft bun with spicy mayo and our signature sauce with fresh lettuce.",
+            price: 195,
+            stock: 10,
+            qty: 1
+        },
+        {
+            id: 2,
+            title: "Krunch Burger with Drink ",
+            image: "/images/products/Chicken-kfc.png",
+            description: "Krunch burger with a regular drink.",
+            price: 265,
+            stock: 3,
+            qty: 1
+        },
+
+    ])
     const [total, setTotal] = useState(0)
-    const incHandler = (index) => {
-        let arr = [...cartdata];
-        arr[index].qty = arr[index].qty + 1
+    const [grandTotal, setGrandTotal] = useState(0)
+
+
+
+    const incHandler = (p) => {
+        let arr = [...qty];
+        p.qty = p.qty + 1
         //console.log(arr)
         setQty(arr)
-        setTotal(total => total + arr[index].price)
+
     }
 
+    const decHandler = (p) => {
+        let arr = [...qty];
+        p.qty = p.qty - 1
+        if (p.qty < 1) {
+            removeCartHandler(p)
+        } else {
+            setQty(arr)
+        }
 
-    function decHandler(index) {
-        let arr = [...cartdata];
-        arr[index].qty = arr[index].qty - 1
-        //console.log(arr)
-        setQty(arr)
-        setTotal(total => total - arr[index].price)
     }
 
     function changeHandler() {
 
     }
+
+    const removeCartHandler = (item) => {
+        let array = qty.filter((p) => p.id != item.id)
+        setQty(array)
+    }
+
     return (
         <>
             <div className="inner-page-wrapper">
@@ -43,46 +76,52 @@ function Cart() {
                 </div>
             </div>
             <section className="cart-items-wrapper pt-5 pb-5">
-                <div className="table-responsive">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col" class="">Image</th>
-                                <th scope="col">Product Name</th>
-                                <th scope="col">price</th>
-                                <th scope="col" style={{ width: "15%" }}>Qty</th>
-                                <th scope="col">Action</th>
-                                <th scope="col">Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                cartdata.map((p, index) => <tr key={index}>
-                                    <td><img src={p.image} alt="" /></td>
-                                    <td>{p.title}</td>
-                                    <td>{p.price}</td>
-                                    <td>
-                                        <div className="input-group">
-                                            <button className="btn btn-outline-secondary" onClick={() => decHandler(index)} type="button">-</button>
-                                            <input type="text" className="form-control" onChange={changeHandler} value={p.qty} />
-                                            <button className="btn btn-outline-secondary" onClick={() => incHandler(index)} type="button">+</button>
-                                        </div>
-                                    </td>
-                                    <td><i className="bi bi-x-square-fill"></i></td>
-                                    <td><strong>PKR {p.price * p.qty}</strong></td>
-                                </tr>)
-                            }
-                        </tbody>
-                    </table>
-                </div>
-                <div className="total-price text-end mb-5">
-                    <span>Total Price: </span>
-                    <span><strong>PKR {total}</strong></span>
-                </div>
-                <div className="cart-exit-btn d-flex justify-content-between">
-                    <a href="#" className="btn btn-primary">continue shopping</a>
-                    <input type="button" className="btn btn-primary" value="checkout" />
-                </div>
+                {
+                    qty.length != 0 ? <>
+                        <div className="table-responsive">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="">Image</th>
+                                        <th scope="col">Product Name</th>
+                                        <th scope="col">price</th>
+                                        <th scope="col" style={{ width: "15%" }}>Qty</th>
+                                        <th scope="col">Action</th>
+                                        <th scope="col">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        qty.map((p) => <tr key={p.title}>
+                                            <td><img src={p.image} alt="" /></td>
+                                            <td>{p.title}</td>
+                                            <td>{p.price}</td>
+                                            <td>
+                                                <div className="input-group">
+                                                    <button className="btn btn-outline-secondary" onClick={() => decHandler(p)} type="button">-</button>
+                                                    <input type="text" className="form-control" onChange={changeHandler} value={p.qty} />
+                                                    <button className="btn btn-outline-secondary" onClick={() => incHandler(p)} type="button">+</button>
+                                                </div>
+                                            </td>
+                                            <td><i className="bi bi-trash-fill" onClick={() => removeCartHandler(p)}></i></td>
+                                            <td><strong>PKR {p.price * p.qty}</strong></td>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="total-price text-end mb-5">
+                            <span>Total Price: </span>
+                            <span><strong>PKR {grandTotal}</strong></span>
+                        </div>
+                        <div className="cart-exit-btn d-flex justify-content-between">
+                            <a href="#" className="btn btn-primary">continue shopping</a>
+                            <input type="button" className="btn btn-primary" value="checkout" />
+                        </div>
+                    </> : <>
+                            <img style={{ display: "block", margin: "0 auto" }} src={cartImg} alt="" />
+                        </>
+                }
             </section>
         </>
     );
